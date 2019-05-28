97f92b0769c982e876835d21d7aad5a4f5d145ec
wangukwa <zacknazareth0@gmail.com>
2019-05-28 20:10:35 +0300

    initial commit

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/master.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,500,600,700&display=swap" rel="stylesheet">
+    <title>404 - coworking space</title>
+  </head>
+  <body>
+    <main class="main-content" id="co-main">
